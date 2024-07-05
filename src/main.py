34dddd92bc5bc1d76fe6b4bcb29d38786ab1b70f
mohamad.alturky <m799678@gmail.com>
2024-07-05 17:14:11 +0300

    adding fastapi starting  point

--- src/main.py
+++ src/main.py
@@ -1,31 +1,18 @@
-from services.tasks import pools_and_swimlanes_extraction, activities_from_pools_and_swimlanes_extraction
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
 
-process_description = """
-        Consider a process for purchasing items from an online shop. The user
-        starts an order by logging in to their account. Then, the user 
-        simultaneously selects the items to purchase and sets a payment
-        method. Afterward, the user either pays or completes an installment 
-        agreement. Since the reward value depends on the purchase value,
-        After selecting the items, the user chooses between multiple options
-        for a free reward. this step is done after selecting the items,
-        but it is independent of the payment activities. Finally, the items
-        are delivered. The user has the right to return items for exchange.
-        Every time items are returned, a new delivery is made.  
+
+@app.get("/")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>🔥🔥 REST SERVER IS RUNNING 🔥🔥</title>
+        </head>
+        <body style="display:flex;justify-content:center;padding-top:100px">
+            <h1>Fastapi🔥🔥BPMN🔥🔥Server Is Running</h1>
+        </body>
+    </html>
     """
-result = pools_and_swimlanes_extraction.evaluate(inputs={
-    "process_description":process_description
-})
-for i in result.pools:
-    print(f"Pool Name: {i["name"]}")
-    for j in i["swimlanes"]:
-        print(f"\t\t{j["name"]}")
-
-activities = activities_from_pools_and_swimlanes_extraction.evaluate(inputs={
-    "process_description":process_description,
-    "pools_and_swimlanes":result
-})
-
-for i in activities.activities:
-    print(f"Activity Name: {i["name"]}")
-    print(f"Activity Pool: {i["pool"]}")
-    print(f"Activity Swimlane: {i["swimlane"]}")
+    return HTMLResponse(content=html_content, status_code=200)
